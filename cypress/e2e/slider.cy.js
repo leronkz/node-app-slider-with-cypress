ca9be68c7f34eecd7598e3e3376a68b6ab1e4dcd
Mateusz Owsiak <41147218+leronkz@users.noreply.github.com>
2024-04-04 15:33:02 +0200

    Update slider.cy.js

--- cypress/e2e/slider.cy.js
+++ cypress/e2e/slider.cy.js
@@ -16,3 +16,27 @@ describe('Swiper Gallery Test', function () {
     cy.get('.swiper-slide-active').should('contain', 'Paris');
   });
 });
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if user can move through slides with navigation buttons', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom');
+    cy.get('.swiper-button-prev').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'Rome');
+  })
+})
+
+describe('Swiper Gallery Test', function () {
+  it('Checks if description of each slide is valid', function () {
+    cy.visit('http://localhost:3000');
+    cy.get('.swiper-slide-active').should('contain', 'Rome', 'Italy');
+    cy.get('.swiper-button-next').click();
+    cy.wait(2000);
+    cy.get('.swiper-slide-active').should('contain', 'United Kingdom', "London");
+    cy.get('.swiper-button-next').click();
+    cy.get('.swiper-slide-active').should('contain', 'Paris', 'France');
+  })
+})
